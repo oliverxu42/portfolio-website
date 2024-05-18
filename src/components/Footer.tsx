ae479d8480e3314662cd8c0e60c85f956fb1e88e
oliverxu42 <xu.oliver8202@gmail.com>
2024-05-18 23:08:16 +1000

    Setup and landing page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className='bg-white border bottom-0'>
+      <div className='w-full mx-auto max-w-screen-xl p-4 flex flex-row-reverse md:items-center md:justify-between'>
+        <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+          © 2024 Oliver Xu. All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+
+interface NavItem {
+  text: string;
+  href: string;
+}
+
+const navItems: NavItem[] = [
+  {
+    text: 'About',
+    href: '/about',
+  },
+  {
+    text: 'Experience',
+    href: '/experience',
+  },
+  {
+    text: 'Projects',
+    href: '/projects',
+  },
+  {
+    text: 'Contact',
+    href: '/contact',
+  },
+];
+
+const Navbar: React.FC = () => {
+  return (
+    <div className='min-h-full'>
+      <nav className='bg-white h-20 p-5 flex items-center border'>
+        <div className='flex items-center m-5 w-screen'>
+          <Link href='/' className='text-black font-bold text-3xl'>
+            Oliver Xu
+          </Link>
+          <div className='flex items-baseline space-x-10 ml-auto'>
+            {navItems.map((n, i) => (
+              <a key={i} href={n.href}>
+                {n.text}
+              </a>
+            ))}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;

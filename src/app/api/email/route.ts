3090293c79a3d943c4cab630aacd38e59177fe4f
oliverxu42 <xu.oliver8202@gmail.com>
2024-05-23 21:01:02 +1000

    Cooked up email route

---
+++ src/app/api/email/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+
+export async function POST(request: NextRequest) {
+  const { email, name, message } = await request.json();
+  const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const mailOptions: Mail.Options = {
+    from: process.env.EMAIL,
+    to: process.env.EMAIL,
+    subject: `Website enquiry from ${name} (${email})`,
+    text: message,
+  };
+
+  console.log(mailOptions, transport);
+
+  try {
+    await transport.sendMail(mailOptions);
+    return NextResponse.json({ message: 'Email sent' });
+  } catch (err) {
+    return NextResponse.json({ error: err }, { status: 500 });
+  }
+}
